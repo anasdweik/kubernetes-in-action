d07b52a360dc991893d768a1a64f87bd5868de8e
anasdweik <a.dweik.99@gmail.com>
2025-01-23 01:03:39 -0500

    Update app.js

--- Chapter02/kubia/app.js
+++ Chapter02/kubia/app.js
@@ -1,14 +1,13 @@
 const http = require('http');
 const os = require('os');
 
-console.log("Kubia server starting...");
+console.log("Updated Kubia server starting...");
 
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
+  response.end("Hello! Prof Lak Adunuthala, It is Anas Aldweik You've hit the server hosted on " + os.hostname() + "\n");
 };
 
 var www = http.createServer(handler);
 www.listen(8080);
-
